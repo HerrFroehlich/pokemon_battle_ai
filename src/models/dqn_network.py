2f3433a29a383f24dfe96fe4e1204e1364e97d8b
Tobias Waurick <twaurick@gmx.net>
2020-09-16 18:43:19 +0200

    Deleted old class

--- src/models/dqn_network.py
+++ src/models/dqn_network.py
@@ -13,34 +13,6 @@ class DQNConfig(object):
     HIDDENLAYER_SIZES = [512,256,32,256,512] #TODO sizes
     HIDDENLAYER_NETWORK_TYPE = torch.nn.Linear # layer type
     ACTIVATION_FUNCTION:object = field(default=torch.nn.functional.relu) # activator function of each layer
-    
-class SelectiveDQN(nn.Module):
-    def __init__(self, config:DQNConfig):
-        super(DQN, self).__init__()
-        self._config = config
-        self._network = DQN(config.D_IN, config, config.D_OUT).to(GLOBAL_TORCH_DEVICE)
-        self._move_ids = torch.LongTensor(config.AVAILABLE_MOVE_IDS)-1 # move ids start with 1
-        # n_moves = len(config.AVAILABLE_MOVE_IDS)
-        # values = torch.ones(n_moves, device=GLOBAL_TORCH_DEVICE)
-        # self._move_mask = torch.sparse_coo_tensor(config.AVAILABLE_MOVE_IDS, values, \
-        #     torch.Size(n_moves), device=GLOBAL_TORCH_DEVICE)
-
-    def set_available_move_ids(ids:List[int]):
-        self._move_ids = torch.LongTensor(ids)-1 # move ids start with 1
-
-
-    def get_max_move(self, state):
-        # only evaluate valid moves
-        filtered_output = self._network(state).index_select(0, self._move_ids)
-        # find the best move of all batches (dim=1)
-        # TODO what if there is none? aka all yield 0,0,0,0 ? -> most likely shouldn't happen thanks to random moves
-
-        # t.max(1) will return largest column value of each row. (so over batches)
-        # second column on max result is index of where max element was
-        # found, so we pick action with the larger expected reward.
-        return filtered_output.max(1)[1].view(1, 1)
-
-
 
 class DQN(nn.Module):
     def __init__(self,config:DQNConfig):
