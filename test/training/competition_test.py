4f346e8329e133e6116fee1047a1ad1c3a03aba0
Tobias Waurick <twaurick@gmx.net>
2020-09-16 10:50:04 +0200

    Added competition test

--- test/training/competition_test.py
+++ test/training/competition_test.py
@@ -1,5 +1,9 @@
-from src.training.competitions import Competition_1vs1, AgentConfig
+from src.training.competitions import Competition_1vs1, CompetitionConfig
 
-conf = AgentConfig()
+conf = CompetitionConfig(N_BATTLES=1000, N_BATTLES_WITH_SAME_TEAM=10)
+
+conf.AGENTCONFIG.BATCH_SIZE = 32
+
+print(conf)
 c = Competition_1vs1(1000, conf)
 c.run()
