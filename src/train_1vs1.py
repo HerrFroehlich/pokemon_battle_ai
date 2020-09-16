fc297cb98098b513a0ad8a36cfddeb1a6c426f91
Tobias Waurick <twaurick@gmx.net>
2020-09-16 20:14:33 +0200

    Save only if save file is stated

--- src/train_1vs1.py
+++ src/train_1vs1.py
@@ -20,10 +20,10 @@ nnconf.ACTIVATION_FUNCTION=torch.nn.functional.relu # activator function of each
 # -------------- Setup Agents
 aconf = AgentConfig(NETWORK_CONFIG=nnconf)
 aconf.MEMORY_SIZE = 10000 # NOF stored states in memory
-aconf.BATCH_SIZE = 128 # NOF batches used for optimizing
-aconf.EPS_START = 0.9 # epsilon start for expontential random decay function
+aconf.BATCH_SIZE = 256 # NOF batches used for optimizing
+aconf.EPS_START = 1 # epsilon start for expontential random decay function
 aconf.EPS_END = 0.05# epsilon end for expontential random decay function
-aconf.EPS_DECAY = 200 # decay gradient for expontential random decay function
+aconf.EPS_DECAY = 2000 # decay gradient for expontential random decay function
 aconf.GAMMA = 0.999
 aconf.TARGET_UPDATE = 200 # after how many turns update the target NN
 aconf.REWARD_FNCT = reward_hp_diff
@@ -34,7 +34,7 @@ aconf.SAVED_STATE_FILE = LOAD_FILE
 # -------------- Setup Competition
 conf = CompetitionConfig(AGENTCONFIG=aconf)
 conf.N_BATTLES = 10000
-conf.N_BATTLES_WITH_SAME_TEAM = 10
+conf.N_BATTLES_WITH_SAME_TEAM = 5
 conf.ENABLE_STATS = True
 conf.STATS_LOG_STEP = 100
 conf.DEBUG = False
@@ -81,9 +81,10 @@ if conf.ENABLE_STATS:
         line.set_label("Cum.  Nof random choices / cum. total")
         ax2[i].legend()
     
-    plt.show()
+if SAVE_FILE:
+    c.get_winner().save_network(SAVE_FILE)
+    with open(SAVE_FILE + "_config", "w") as f:
+        f.write(str(conf))
+        f.close()
 
-c.get_winner().save_network(SAVE_FILE)
-with open("config_"+SAVE_FILE, "w") as f:
-    f.write(conf)
-    f.close()
+plt.show()
